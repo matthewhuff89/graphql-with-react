38e9faa2551c00441d2e35abed41949238c72520
Matthew D. Huff <matthewhuff89@gmail.com>
2017-12-21 15:25:51 -0500

    First commit

---
+++ users/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
+
+app.listen(3955, () => {
+  console.log('Listening');
+});
