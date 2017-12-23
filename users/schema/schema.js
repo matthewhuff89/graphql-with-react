082f45da398fadb079a1bbbd353c51ff7593022a
Matthew D. Huff <matthewhuff89@gmail.com>
2017-12-23 14:16:39 -0600

    Add editUser mutation

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -22,7 +22,7 @@ const CompanyType = new GraphQLObjectType({
         return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
           .then(response => response.data);
       }
-    } 
+    }
   })
 });
 
@@ -89,6 +89,19 @@ const mutation = new GraphQLObjectType({
       },
       resolve(parentValue, { id }) {
         return axios.delete(`http://localhost:3000/users/${id}`)
+          .then(response => response.data);
+      }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, { id, firstName, age, companyId }) {
+        return axios.patch(`http://localhost:3000/users/${id}`, { firstName, age, companyId })
           .then(response => response.data);
       }
     }
